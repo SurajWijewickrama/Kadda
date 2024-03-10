4bc4666b40c6dd9fb6c066a58c838b668cd16800
SurajWijewickrama <wijewickramasuraj@gmail.com>
2024-03-10 17:26:27 +0530

    api

--- src/api/assistant.ts
+++ src/api/assistant.ts
@@ -1,14 +1,17 @@
 const askAssistant = async (prompt: string) => {
-  const res = await fetch(`${process.env.BACKEND_URL}/assistant`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: prompt,
-    }),
-  });
+  const res = await fetch(
+    `https://fastapi-production-2aab.up.railway.app/assistant`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+      }),
+    }
+  );
   return res.json();
 };
 
